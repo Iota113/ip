7946d6d93a7278d69c63e5803a190989f4850027
Iota113 <henrytse113@gmail.com>
2026-01-22 00:10:00 +0800

    Add echo features

--- src/main/java/Sandrone.java
+++ src/main/java/Sandrone.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class Sandrone {
     public static void main(String[] args) {
-        String greetings = "____________________________________________________________\n" +
+        String greetings =
                 " Hello! I'm Sandrone\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+                " What can I do for you?\n";
+        String farewell = " Bye. Hope to see you again soon!\n";
+
         System.out.println(greetings);
+
+        Scanner scn = new Scanner(System.in);
+        String sentence = scn.nextLine();
+        while (!sentence.equals("bye")) {
+            System.out.println(sentence);
+            sentence = scn.nextLine();
+        }
+
+        System.out.println(farewell);
     }
 }
