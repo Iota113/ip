ce346f1b7fa2868a92d47be0c60b5fa4be2c6f7f
Iota113 <henrytse113@gmail.com>
2026-01-21 23:43:44 +0800

    Refactor Duke to Sandrone

---
+++ src/main/java/Sandrone.java
@@ -0,0 +1,11 @@
+public class Sandrone {
+    public static void main(String[] args) {
+        String greetings = "____________________________________________________________\n" +
+                " Hello! I'm Sandrone\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(greetings);
+    }
+}
