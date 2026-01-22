43193fbd901b7eac3d3a3368f4153cf044063890
Iota113 <henrytse113@gmail.com>
2026-01-22 19:37:15 +0800

    Add exception handling for event task

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,11 +2,30 @@ public class Event extends Task{
     private String from;
     private String to;
 
-    public Event(String input) {
+    public Event(String input) throws SandroneException {
         super(input);
-        this.desc = input.split(" /from ")[0].replace("event ", "");
-        this.from = input.split(" /from ")[1].split( " /to ")[0];
-        this.to = input.split(" /to ")[1];
+        if (!(input.contains(" /from ") && input.contains(" /to"))) {
+            throw new SandroneException("Incomplete command! An Event needs a ' /from ' and ' /to ' component.");
+        }
+
+        // Remove "event"
+        input = input.substring(5).trim();
+        String[] desc_part = input.split("/from");
+        this.desc = desc_part[0].trim();
+        if (this.desc.isEmpty()) {
+            throw new SandroneException("The task description is empty!");
+        }
+
+
+        String[] time_parts = desc_part[1].split("/to");
+        if (time_parts.length < 2) {
+            if (time_parts[0].trim().isEmpty()) throw new SandroneException("Both from and to fields are empty!");
+            throw new SandroneException("The to field is empty!");
+        }
+        this.from = time_parts[0].trim();
+        this.to = time_parts[1].trim();
+        if (this.from.isEmpty()) throw new SandroneException("The from field is empty!");
+
     }
 
     @Override
