1ada60c915b4856137f09b1011acc0f9cd42162a
Iota113 <henrytse113@gmail.com>
2026-01-28 01:19:29 +0800

    Adjust code to follow Java Standards

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -21,45 +21,45 @@ public class TaskList {
         Action action = Action.fromCommand(input);
         Task newTask = null;
         switch (action) {
-            case LIST:
-                printList();
-                return "";
-            case MARK:
-                return setTaskStatus(Pulonia.extractIndex(input), true);
-            case UNMARK:
-                return setTaskStatus(Pulonia.extractIndex(input), false);
-            case DELETE:
-                return deleteTask(Pulonia.extractIndex(input));
-            case TODO:
-                newTask = new Todo(input);
-                break;
-            case DEADLINE:
-                newTask = new Deadline(input);
-                break;
-            case EVENT:
-                newTask = new Event(input);
-                break;
-            default:
-                String message =
-                        "You Fool. What are you saying.\n"
-                        + "Here are the valid commands:\n"
-                        + "1. todo [insert desc] \n"
-                        + "2. deadline [insert desc] /by [insert time]\n"
-                        + "3. event [insert desc] /from [insert time] /to [insert time]\n"
-                        + "4. mark/unmark [insert index]\n"
-                        + "5. list";
-                throw new SandroneException(message);
+        case LIST:
+            printList();
+            return "";
+        case MARK:
+            return setTaskStatus(Pulonia.extractIndex(input), true);
+        case UNMARK:
+            return setTaskStatus(Pulonia.extractIndex(input), false);
+        case DELETE:
+            return deleteTask(Pulonia.extractIndex(input));
+        case TODO:
+            newTask = new Todo(input);
+            break;
+        case DEADLINE:
+            newTask = new Deadline(input);
+            break;
+        case EVENT:
+            newTask = new Event(input);
+            break;
+        default:
+            String message =
+                    "You Fool. What are you saying.\n"
+                            + "Here are the valid commands:\n"
+                            + "1. todo [insert desc] \n"
+                            + "2. deadline [insert desc] /by [insert time]\n"
+                            + "3. event [insert desc] /from [insert time] /to [insert time]\n"
+                            + "4. mark/unmark [insert index]\n"
+                            + "5. list";
+            throw new SandroneException(message);
         }
 
-        if (newTask!=null) tasks.add(newTask);
+        if (newTask != null) tasks.add(newTask);
         int count = tasks.size();
-        String addTaskmessage =
+        String addTaskMessage =
                 "Very well. You have " + count + " task(s) now.\n" +
                         count + "."
-                        + "[" + tasks.get(count-1).getStatusIcon() + "]"
-                        + "[" + tasks.get(count-1).getTaskType() + "] "
-                        + tasks.get(count-1).getDescription();
-        return addTaskmessage;
+                        + "[" + tasks.get(count - 1).getStatusIcon() + "]"
+                        + "[" + tasks.get(count - 1).getTaskType() + "] "
+                        + tasks.get(count - 1).getDescription();
+        return addTaskMessage;
     }
 
     public String setTaskStatus(int id, boolean isMark) {
@@ -85,7 +85,7 @@ public class TaskList {
         System.out.println("Your List:");
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println(
-                    (i+1) + "."
+                    (i + 1) + "."
                             + "[" + tasks.get(i).getStatusIcon() + "]"
                             + "[" + tasks.get(i).getTaskType() + "] "
                             + tasks.get(i).getDescription());
